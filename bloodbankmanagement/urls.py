1513f45e5c931151da3d0f404f1bcf1aabd5dd18
Shuba0S <shuba9902@gmail.com>
2025-08-25 17:31:59 +0530

    added middleware

--- bloodbankmanagement/urls.py
+++ bloodbankmanagement/urls.py
@@ -13,10 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from xml.dom.minidom import Document
 from django.contrib import admin
 from django.urls import path,include
 from django.contrib.auth.views import LogoutView,LoginView
 from blood import views
+from django.conf.urls.static import static
+from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
 
@@ -48,3 +51,5 @@ urlpatterns = [
     path('contact/', include('contact.urls')),
    
 ]
+urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns+=static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
